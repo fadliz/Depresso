8e6bd045398dc054e2c95272da10592f29277629
fadliz <asama.fadli@gmail.com>
2022-07-25 21:28:37 +0700

    Ver 2 of grounded detector + sprite cleaning
    Version 2 of ground detector via BoxCast and Cleaning sprite A.K.A. disable anti aliasing and made pixel 48

--- My project/Assets/Scenes/Scripts/PlayerMove.cs
+++ My project/Assets/Scenes/Scripts/PlayerMove.cs
@@ -4,32 +4,22 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-/*     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    } */
-
     [SerializeField] private float speed;
-    //[SerializeField] private float maxspeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private float fallThreshold;
     private Rigidbody2D body;
+    private BoxCollider2D boxCollider2d;
     private Animator anim;
-    private bool grounded;
     private bool falling;
-    private int countCol;
+    private bool tofall;
 
     private void Awake()
     {
         //grab reference to rigidbody & animator
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -37,53 +27,79 @@ public class PlayerMove : MonoBehaviour
         //horizontal input move rigidbody times speed
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        if (body.velocity.y < -fallThreshold)
+        {
+            body.velocity = new Vector2(body.velocity.x, -fallThreshold);
+        }
 
 
         //check direction, bigger than float 0.01 is right direction
         if (horizontalInput > 0.01f)
+        {
             transform.localScale = Vector3.one;
+        }
         else if (horizontalInput < -0.01f)
+        {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         //jump
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             Jump();
-
-        if (!grounded && body.velocity.y < -0.01f)
-            falling = true;
+        }
 
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
-        anim.SetBool("falling", falling);
-        Debug.Log(countCol);
+        anim.SetBool("grounded", isGrounded());
+        anim.SetBool("falling", isFalling());
+        anim.SetBool("tofall", tofall);
     }
 
-    private void Jump()
+    private bool isFalling()
     {
-        body.velocity = new Vector2(body.velocity.x, speed * 2);
-        anim.SetTrigger("jump");
-        grounded = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) 
-    {
-        if(collision.gameObject.tag == "Ground")
-            countCol++;
-            grounded = true;
-            falling = false;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision) 
-    {
-        if(collision.gameObject.tag == "Ground" )
-            if(countCol > 1)
-                countCol--;
+        if (!isGrounded() && (body.velocity.y > -1.5f && body.velocity.y < 1.5f))
+        {
+            tofall = true;
             
-            if(countCol == 1)
-                grounded = false;
+        }
+        if (!isGrounded() && body.velocity.y < -0.5f)
+        {
+            falling = true;
+            tofall = false;
+        }
+        else
+        {
+            falling = false;
+        }
+        return falling;
     }
 
-/*     private bool isGrounded() {
-       return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded;
-    } */
+        private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+    }
+
+    private bool isGrounded()
+    {
+        float extraHeightText = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, extraHeightText, platformLayerMask);
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        } 
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(boxCollider2d.bounds.center + new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, 0), Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(boxCollider2d.bounds.center - new Vector3(boxCollider2d.bounds.extents.x, boxCollider2d.bounds.extents.y + extraHeightText), Vector2.right * (boxCollider2d.bounds.extents.x), rayColor);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
 }
